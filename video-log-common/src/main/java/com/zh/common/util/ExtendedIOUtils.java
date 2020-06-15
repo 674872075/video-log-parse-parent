3d3c3c9134725b206b670187d71c871a3c66e145
Tourist <674872075@qq.com>
2020-06-15 19:06:18 +0800

    提交人 : zhouhao 描述 : 公共模块，新增HttpClient工具类 是否测试:已测试通过

--- video-log-common/src/main/java/com/zh/common/util/ExtendedIOUtils.java
+++ video-log-common/src/main/java/com/zh/common/util/ExtendedIOUtils.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 /**
  * IO流拓展工具类，补充IOUtils新版本中废弃的closeQuietly
+ *
  * @author King
  * @since 2018/12/27 17:56
  */
@@ -15,9 +16,7 @@ public class ExtendedIOUtils {
 
     public static void flush(Flushable... resources) throws IOException {
         Assert.noNullElements(resources, "resources invalid");
-        int length = resources.length;
-        for(int i = 0; i < length; ++i) {
-            Flushable resource = resources[i];
+        for (Flushable resource : resources) {
             if (resource != null) {
                 resource.flush();
             }
@@ -25,9 +24,7 @@ public class ExtendedIOUtils {
     }
 
     public static void closeQuietly(Closeable... resources) {
-        int length = resources.length;
-        for(int i = 0; i < length; ++i) {
-            Closeable resource = resources[i];
+        for (Closeable resource : resources) {
             if (resource != null) {
                 try {
                     resource.close();
